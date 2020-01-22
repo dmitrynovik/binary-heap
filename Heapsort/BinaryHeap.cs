b9e30c693f73d2df7c63840401afbcc6360de5c8
Dmitry Novik <dmitry.novik@gmail.com>
2020-01-22 14:46:26 +1100

    Implemented Left, Right

---
+++ Heapsort/BinaryHeap.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace DataStructures.BinaryHeap
+{
+    public class BinaryHeap<T>
+    {
+        private IList<T> _list;
+
+
+        public BinaryHeap() {  _list = new List<T>(); }
+
+        public BinaryHeap(int capacity) {  _list = new List<T>(capacity); }
+
+        public int HeapSize => _list.Count;
+
+        public T GetParent(int i) => i == 0 ? default(T) : _list[GetParentIndex(i)];
+
+        public T GetLeft(int i) => GetAt(GetLeftIndex(i));
+
+        public T GetRight(int i) => GetAt(GetRightIndex(i));
+
+
+        internal static int GetParentIndex(int i) => (i - 1) >> 1;
+
+        internal static int GetLeftIndex(int i) => ((i + 1) << 1) - 1;
+
+        internal static int GetRightIndex(int i) => ((i + 1) << 1);
+
+        private T GetAt(int index) => index < _list.Count ? _list[index] : default(T);
+    }
+}
---
+++ Heapsort/BinaryHeapTest.cs
@@ -0,0 +1,34 @@
+﻿using FluentAssertions;
+using FluentAssertions.Execution;
+using NUnit.Framework;
+
+namespace DataStructures.BinaryHeap
+{
+    [TestFixture]
+    public class BinaryHeapTest
+    {
+        [Test]
+        public void Index_Even_Produces_Correct_Parent_Left_Right()
+        {
+            var self = 2;
+            using (new AssertionScope())
+            {
+                BinaryHeap<int>.GetParentIndex(self).Should().Be(0);
+                BinaryHeap<int>.GetLeftIndex(self).Should().Be(5);
+                BinaryHeap<int>.GetRightIndex(self).Should().Be(6);
+            }
+        }
+
+        [Test]
+        public void Index_Odd_Produces_Correct_Parent_Left_Right()
+        {
+            var self = 1;
+            using (new AssertionScope())
+            {
+                BinaryHeap<int>.GetParentIndex(self).Should().Be(0);
+                BinaryHeap<int>.GetLeftIndex(self).Should().Be(3);
+                BinaryHeap<int>.GetRightIndex(self).Should().Be(4);
+            }
+        }
+    }
+}
